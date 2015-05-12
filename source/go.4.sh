c5f70d813b47b6ab3b167970a3b0cf3b4ff55891
Darren Mulholland <dmulholland@outlook.ie>
2015-05-12 18:10:23 +0100

    Test if the Go binary is installed before invoking it

--- source/go.4.sh
+++ source/go.4.sh
@@ -3,5 +3,10 @@
 # Go
 # ----------------------------------------------------------------------------
 
-export GOROOT=`go env GOROOT`
-export GOPATH=~/dev/go
+if type go &> /dev/null; then
+    is_init && e_check " .. Go installation found"
+    export GOROOT=`go env GOROOT`
+    export GOPATH=~/dev/go
+else
+    is_init && e_error " .. no Go installation found"
+fi
