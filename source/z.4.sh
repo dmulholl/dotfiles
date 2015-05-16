f6331a69210d2f3d10707b6b28f035155a3ebc61
Darren Mulholland <dmulholland@outlook.ie>
2015-05-16 14:59:15 +0100

    Use 'j' as the jump command instead of 'z'

--- source/z.4.sh
+++ source/z.4.sh
@@ -3,4 +3,8 @@
 # z config
 # -------------------------------------------------------------------------
 
+# Use 'j' as the jump command instead of the default 'z'.
+export _Z_CMD="j"
+
+# Load the 'z' library.
 source $DOTFILES/lib/z.sh
