2dcb8edc96f942af6bc251737ac076d7eeeda873
Darren Mulholland <dmulholland@outlook.ie>
2015-05-17 22:35:11 +0100

    Fix name of logging function

--- source/go.4.sh
+++ source/go.4.sh
@@ -7,5 +7,5 @@ if is_installed go; then
     export GOROOT=`go env GOROOT`
     export GOPATH=~/dev/go
 else
-    [[ $verbose ]] && e_error " .. no Go installation found"
+    [[ $verbose ]] && df_error " .. no Go installation found"
 fi
