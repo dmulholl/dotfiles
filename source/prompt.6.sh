9bab8ffcfd509895f75cac598940159e0e4c5613
Darren Mulholland <dmulholland@outlook.ie>
2015-05-29 15:40:09 +0100

    Update prompt colours

--- source/prompt.6.sh
+++ source/prompt.6.sh
@@ -6,14 +6,24 @@
 # Echo the currently active Python virtual environment.
 function prompt_virtualenv() {
     if [ -n "$VIRTUAL_ENV" ]; then
-        echo "($(basename $VIRTUAL_ENV)) "
+        echo "[$(basename $VIRTUAL_ENV)] "
     fi
 }
+
+# Default prompt colours.
+venvcolour=$purple
+usercolour=$green
+pathcolour=$yellow
+
+# Prompt colours on Linux.
+if is_linux; then
+    usercolour=$cyan
+fi
 
 # Set the window title.
 title "$USER@$HOSTNAME"
 
 # Set the prompt.
 export PS1="
-\[$purple\]\$(prompt_virtualenv)\[$green\]\u@\h \[$yellow\]\w
+\[$venvcolour\]\$(prompt_virtualenv)\[$usercolour\]\u@\h \[$pathcolour\]\w
 \[$normal\]\!: \$ "
