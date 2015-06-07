3041c28d62b678f46cebff5ad6c245345919885f
Darren Mulholland <dmulholland@outlook.ie>
2015-06-07 11:42:53 +0100

    Add Go binaries to PATH only when Go is installed

--- source/path.6.sh
+++ source/path.6.sh
@@ -3,10 +3,23 @@
 # Path
 # -------------------------------------------------------------------------
 
-      PATH=$DOTFILES/bin
-PATH=$PATH:~/dev/bin
-PATH=$PATH:$GOPATH/bin
-PATH=$PATH:$GOROOT/bin
-PATH=$PATH:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
+# Dotfiles binaries.
+PATH=$DOTFILES/bin
 
+# Development binaries.
+PATH=$PATH:~/dev/bin
+
+# Go binaries.
+if [[ -n "$GOPATH" ]]; then
+    PATH=$PATH:$GOPATH/bin
+    PATH=$PATH:$GOROOT/bin
+fi
+
+# Locally installed binaries.
+PATH=$PATH:/usr/local/bin
+
+# System binaries.
+PATH=$PATH:/usr/bin:/bin:/usr/sbin:/sbin
+
+# Make it so.
 export PATH
