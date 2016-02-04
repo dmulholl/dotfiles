1733602a7febb618f70422377b9fb97f9b9898ae
Darren Mulholland <dmulholland@outlook.ie>
2016-02-04 13:34:47 +0000

    $ARK_EXTENSIONS -> $ARK_EXT

--- source/env.4.sh
+++ source/env.4.sh
@@ -3,7 +3,7 @@
 # --------------------------------------------------------------------------
 
 # Global Ark extensions directory.
-export ARK_EXTENSIONS=~/dev/www/arkplugins
+export ARK_EXT=~/dev/www/arkplugins
 
 # Global Ark themes directory.
 export ARK_THEMES=~/dev/src/themes
