a3bcec513a1abaf6a4a38a671691fb5dab0aa0e8
Darren Mulholland <darren@mulholland.xyz>
2016-06-24 12:26:56 +0100

    Update ARK_EXT location

--- source/env.4.sh
+++ source/env.4.sh
@@ -3,7 +3,7 @@
 # --------------------------------------------------------------------------
 
 # Global Ark extensions directory.
-export ARK_EXT=~/dev/src/ark-plugins/global
+export ARK_EXT=~/dev/src/ark-plugins
 
 # Global Ark themes directory.
 export ARK_THEMES=~/dev/src/ark-themes
