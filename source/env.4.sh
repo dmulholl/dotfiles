317024a3d076224f527d0337b51c4120321dde92
Darren Mulholland <darren@mulholland.xyz>
2017-03-30 14:15:11 +0100

    Remove ark themes env variable

--- source/env.4.sh
+++ source/env.4.sh
@@ -1,9 +1,3 @@
 # --------------------------------------------------------------------------
 # Environment variables.
 # --------------------------------------------------------------------------
-
-# Global Ark extensions directory.
-# export ARK_EXT=~/dev/src/ark-plugins
-
-# Global Ark themes directory.
-export ARK_THEMES=~/dev/src/ark-themes
