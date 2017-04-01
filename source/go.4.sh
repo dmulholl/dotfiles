ceeff0f491970c66c33ab18c8dd4c694d75eb56b
Darren Mulholland <darren@mulholland.xyz>
2017-04-01 18:11:23 +0100

    Update GOPATH to home

--- source/go.4.sh
+++ source/go.4.sh
@@ -4,7 +4,7 @@
 
 if is_installed go; then
     export GOROOT=`go env GOROOT`
-    export GOPATH=~/dev
+    export GOPATH=$HOME
 else
     [[ $verbose ]] && dot_arrow " .. no Go installation found"
 fi
