f9a7e0d67d74bd07461304e215db4400743f2dd6
Darren Mulholland <darren@mulholland.xyz>
2017-09-07 23:50:40 +0100

    Add dashes()

--- source/functions.2.sh
+++ source/functions.2.sh
@@ -91,3 +91,11 @@ function man() {
         LESS_TERMCAP_us=$(printf "\e[1;32m") \
         man "$@"
 }
+
+# Print a line of dashes across the width of the terminal.
+function dashes() {
+    for (( i=0; i<$COLUMNS; i++ )); do
+        echo -n "-"
+    done
+    echo
+}
