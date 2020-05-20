c126ffae8bc149d30f7eb43b35c25064c56ff344
Darren Mulholland <dmulholl@tcd.ie>
2020-05-20 22:07:50 +0100

    Move z script to /source

--- source/env.sh
+++ source/env.sh
@@ -3,6 +3,6 @@
 # ------------------------------------------------------------------------------
 
 export LESS="--tabs=4 -RFX"
-
 export FZF_DEFAULT_COMMAND="rg --files"
-
+export _Z_CMD="j"
+export _Z_DATA="$HOME/.cache/zdata"
