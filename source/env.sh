1dd4cae04a813a4e75000eba9fd2922fcbb265d1
Darren Mulholland <dmulholl@tcd.ie>
2021-05-13 16:03:15 +0100

    Housekeeping

--- source/env.sh
+++ source/env.sh
@@ -3,7 +3,7 @@
 # ------------------------------------------------------------------------------
 
 export LESS="--tabs=4 -RFX"
-export FZF_DEFAULT_COMMAND="rg --files -g '!out/'"
+export FZF_DEFAULT_COMMAND="rg --files --no-ignore -g '!out/'"
 export _Z_CMD="jj"
 export _Z_DATA="$HOME/.cache/zdata"
 export EDITOR="vim"
