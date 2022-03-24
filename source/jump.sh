ec48f977a9faec637e67474285d8853349632ed3
Darren Mulholland <dmulholl@tcd.ie>
2022-03-24 23:11:34 +0000

    Housekeeping

--- source/jump.sh
+++ source/jump.sh
@@ -12,6 +12,8 @@ function j() {
             cd ~/dev/bin;;
         dm)
             cd ~/dev/web/dmulholl.com;;
+        tmp)
+            cd ~/dev/tmp;;
         vay)
             cd ~/dev/vay;;
         vim)
