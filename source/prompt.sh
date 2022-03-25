1dd9b6efb465873b5538f743cf97b7ca81c90168
Darren Mulholland <dmulholl@tcd.ie>
2022-03-25 11:33:07 +0000

    Add prompt() function

--- source/prompt.sh
+++ source/prompt.sh
@@ -18,14 +18,14 @@ print_git_dirty() {
 
 # Print the exit code from the last process, the python virtual environment,
 # and git branch name and status.
-print_prompt_data() {
+print_prompt_meta() {
     echo -n "[$?"
 
     # Python enviroment.
     if [ $CONDA_PREFIX ]; then
         echo -n ":conda($(basename $CONDA_DEFAULT_ENV))"
     elif [ $VIRTUAL_ENV ]; then
-        echo -n ":dotpy($(basename $VIRTUAL_ENV))"
+        echo -n ":$(basename $VIRTUAL_ENV)"
     fi
 
     # Git branch name.
@@ -65,16 +65,29 @@ print_arrows() {
     fi
 }
 
-simple_prompt="
-\[$fgc_magenta\](--:--) \[$fgc_green\]\u \[$fgc_yellow\]\w
-\[$fgc_yellow\] >> \[$fgc_default\]"
+print_prompt_user() {
+    username="$USER"
+    hostname=$(hostname -s | tr '[:upper:]' '[:lower:]')
 
-mac_prompt="
-\[$fgc_magenta\](--:--) \$(print_prompt_data) \[$fgc_green\]\u@\h \[$fgc_yellow\]\w
+    if [[ $hostname == vay* ]]; then
+        echo "(vay)"
+    elif [[ $hostname == "mbp16" && $username == "dmulholl" ]]; then
+        echo "(mbp16)"
+    else
+        echo "$username@$hostname"
+    fi
+}
+
+short_prompt="
+\[$fgc_magenta\](--:--) \[$fgc_green\]\u \[$fgc_yellow\]\w
 \[$fgc_yellow\] \$(print_arrows) \[$fgc_default\]"
 
-linux_prompt="
-\[$fgc_magenta\](--:--) \$(print_prompt_data) \[$fgc_blue\]\u@\h \[$fgc_yellow\]\w
+default_prompt="
+\[$fgc_magenta\](--:--) \$(print_prompt_meta) \[$fgc_green\]$(print_prompt_user) \[$fgc_yellow\]\w
+\[$fgc_yellow\] \$(print_arrows) \[$fgc_default\]"
+
+long_prompt="
+\[$fgc_magenta\](--:--) \$(print_prompt_meta) \[$fgc_green\]\u@\h \[$fgc_yellow\]\w
 \[$fgc_yellow\] \$(print_arrows) \[$fgc_default\]"
 
 # Bash expands and displays PS0 after it reads a command but before executing it.
@@ -82,11 +95,41 @@ linux_prompt="
 # then jumps back to the saved position.
 export PS0="\$(tput sc)\$(move_to_start_of_ps1)\[$fgc_magenta\](\A)\[$fgc_default\]\$(tput rc)"
 
-export PS2="\[$fgc_yellow\]\$(print_num_jobs) >> \[$fgc_default\]"
+# This is the continuation prompt for multi-line commands.
+export PS2="\[$fgc_yellow\] \$(print_arrows) \[$fgc_default\]"
 
-if is_linux; then
-    export PS1="$linux_prompt"
-else
-    # export PS1="$simple_prompt"
-    export PS1="$mac_prompt"
-fi
+function prompt_help {
+    cat <<EOF
+Usage: prompt <style>
+
+  Sets the prompt style.
+
+Styles:
+  - short/simple
+  - default
+  - long/user/username
+EOF
+}
+
+function prompt {
+    if [[ -n "$1" ]]; then
+        local command="$1"
+        shift
+        case "$command" in
+            -h|--help)
+                prompt_help;;
+            short|simple)
+                export PS1="$short_prompt";;
+            default)
+                export PS1="$default_prompt";;
+            long|user|username)
+                export PS1="$long_prompt";;
+            *)
+                prompt_help;;
+        esac
+    else
+        export PS1="$default_prompt"
+    fi
+}
+
+export PS1="$default_prompt"
