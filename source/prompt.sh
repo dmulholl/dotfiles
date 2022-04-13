3f65b286b8a6f7d3687f62b99f537da47b88fcbb
Darren Mulholland <dmulholl@tcd.ie>
2022-04-13 18:48:36 +0100

    Housekeeping

--- source/prompt.sh
+++ source/prompt.sh
@@ -6,18 +6,18 @@
 # actual length of the prompt.
 # ------------------------------------------------------------------------------
 
-# Print the name of the current git branch.
-print_git_banch_name() {
+# Prints the name of the current git branch.
+dot_print_git_branch_name() {
     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
 }
 
-# Print '*' if the current git branch is dirty.
-print_git_is_dirty() {
+# Prints '*' if the current git branch is dirty.
+dot_print_git_is_dirty() {
     [[ -n $(git status --porcelain 2> /dev/null) ]] && echo "*"
 }
 
-# Print metadata about the current shell environment.
-print_prompt_meta() {
+# Prints metadata about the current shell environment.
+dot_print_prompt_meta() {
     # The exit code from the last process.
     echo -n "[$?"
 
@@ -29,8 +29,8 @@ print_prompt_meta() {
     fi
 
     # The current git branch name.
-    local branch_name="$(print_git_banch_name)"
-    local is_dirty="$(print_git_is_dirty)"
+    local branch_name="$(dot_print_git_branch_name)"
+    local is_dirty="$(dot_print_git_is_dirty)"
 
     if test ! -z "$branch_name"; then
         echo -n ":${branch_name}${is_dirty}"
@@ -39,11 +39,39 @@ print_prompt_meta() {
     echo -n "]"
 }
 
-# This command moves the cursor into the correct position to overwrite the blank (--:--) time
-# template.
+# Prints user@host or an abbreviated version for known systems.
+dot_print_prompt_user() {
+    local user_name="$USER"
+    local host_name=$(hostname -s | tr '[:upper:]' '[:lower:]')
+
+    if [[ $host_name == vay* ]]; then
+        echo "(vay)"
+    elif [[ $host_name == "mbp16" && $user_name == "dmulholl" ]]; then
+        echo "(mbp16)"
+    else
+        echo "$user_name@$host_name"
+    fi
+}
+
+# Prints the number of jobs currently in the background.
+dot_print_num_background_jobs() {
+    jobs -p | wc -l | tr -d " "
+}
+
+# Prints 'n>' where n is the number of background jobs, or '>>' if n is zero.
+dot_print_prompt_arrows() {
+    # The number of jobs is always 1 too many when the function runs inside the prompt.
+    if [[ "$(dot_print_num_background_jobs)" -gt "1" ]]; then
+        echo -n "$(($(dot_print_num_background_jobs) - 1))>"
+    else
+        echo -n ">>"
+    fi
+}
+
+# Moves the cursor into the correct position to overwrite the blank (--:--) time template.
 # - For single-line commands, we allow for line-wrapping.
 # - For multi-line commands, we make a best effort and ignore the possibility of line-wrapping.
-move_to_start_of_ps1() {
+dot_move_to_start_of_ps1() {
     local num_lines_in_last_command=$(history 1 | wc -l)
 
     if [ $num_lines_in_last_command -gt 1 ]; then
@@ -60,50 +88,22 @@ move_to_start_of_ps1() {
     tput cuu $lines_to_move
 }
 
-# Prints the number of jobs currently in the background.
-print_num_jobs() {
-    jobs -p | wc -l | tr -d " "
-}
-
-# Prints 'n>' where n is the number of background jobs, or '>>' if n is zero.
-print_arrows() {
-    # The number of jobs is always 1 too many when the function runs inside the prompt.
-    if [[ "$(print_num_jobs)" -gt "1" ]]; then
-        echo -n "$(($(print_num_jobs) - 1))>"
-    else
-        echo -n ">>"
-    fi
-}
-
-print_prompt_user() {
-    local user_name="$USER"
-    local host_name=$(hostname -s | tr '[:upper:]' '[:lower:]')
-
-    if [[ $host_name == vay* ]]; then
-        echo "(vay)"
-    elif [[ $host_name == "mbp16" && $user_name == "dmulholl" ]]; then
-        echo "(mbp16)"
-    else
-        echo "$user_name@$host_name"
-    fi
-}
-
 short_prompt="
 \[$fgc_magenta\](--:--) \[$fgc_green\]\u \[$fgc_yellow\]\w
-\[$fgc_yellow\] \$(print_arrows) \[$fgc_default\]"
+\[$fgc_yellow\] \$(dot_print_prompt_arrows) \[$fgc_default\]"
 
 default_prompt="
-\[$fgc_magenta\](--:--) \$(print_prompt_meta) \[$fgc_green\]$(print_prompt_user) \[$fgc_yellow\]\w
-\[$fgc_yellow\] \$(print_arrows) \[$fgc_default\]"
+\[$fgc_magenta\](--:--) \$(dot_print_prompt_meta) \[$fgc_green\]$(dot_print_prompt_user) \[$fgc_yellow\]\w
+\[$fgc_yellow\] \$(dot_print_prompt_arrows) \[$fgc_default\]"
 
 long_prompt="
-\[$fgc_magenta\](--:--) \$(print_prompt_meta) \[$fgc_green\]\u@\h \[$fgc_yellow\]\w
-\[$fgc_yellow\] \$(print_arrows) \[$fgc_default\]"
+\[$fgc_magenta\](--:--) \$(dot_print_prompt_meta) \[$fgc_green\]\u@\h \[$fgc_yellow\]\w
+\[$fgc_yellow\] \$(dot_print_prompt_arrows) \[$fgc_default\]"
 
 # Bash expands and displays PS0 after it reads a command but before executing it.
 # This PS0 saves the cursor position, jumps up and overwrites the time placeholder,
 # then jumps back to the saved position.
-export PS0="\$(tput sc)\$(move_to_start_of_ps1)\[$fgc_magenta\](\A)\[$fgc_default\]\$(tput rc)"
+export PS0="\$(tput sc)\$(dot_move_to_start_of_ps1)\[$fgc_magenta\](\A)\[$fgc_default\]\$(tput rc)"
 
 # This is the continuation prompt for multi-line commands.
 export PS2="\[$fgc_yellow\] >> \[$fgc_default\]"
