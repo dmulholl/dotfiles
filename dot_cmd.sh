8eecbbb9636b6fd562234c19a875a7a5ff7c2816
Darren Mulholland <dmulholl@tcd.ie>
2023-01-23 21:32:09 +0000

    Housekeeping

--- dot_cmd.sh
+++ dot_cmd.sh
@@ -69,6 +69,7 @@ dot_source() {
     source ~/.dotfiles/source/z.sh
     source ~/.dotfiles/source/prompt.sh
     source ~/.dotfiles/source/jump.sh
+    source ~/.dotfiles/source/git-completion.bash
 }
 
 dot_source_help() {
--- source/bash-completion.sh
+++ source/bash-completion.sh
@@ -3,12 +3,11 @@
 # ------------------------------------------------------------------------------
 
 # Git-completion -- Homebrew on Apple silicon
-if [ -f /opt/homebrew/etc/bash_completion.d/git-completion.bash ]; then
-    . /opt/homebrew/etc/bash_completion.d/git-completion.bash
-fi
+# if [ -e /opt/homebrew/etc/bash_completion.d/git-completion.bash ]; then
+#     source /opt/homebrew/etc/bash_completion.d/git-completion.bash
+# fi
 
 # Git-completion -- Homebrew on Apple Intel.
-if [ -f /usr/local/etc/bash_completion.d/git-completion.bash ]; then
-    . /usr/local/etc/bash_completion.d/git-completion.bash
-fi
-
+# if [ -e /usr/local/etc/bash_completion.d/git-completion.bash ]; then
+#     source /usr/local/etc/bash_completion.d/git-completion.bash
+# fi
