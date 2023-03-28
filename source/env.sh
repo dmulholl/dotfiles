b9e343d97e25b8d8e67115c5372b7d8962350830
Darren Mulholland <dmulholl@tcd.ie>
2023-03-28 08:34:38 +0100

    Housekeeping

--- source/env.sh
+++ source/env.sh
@@ -3,7 +3,6 @@
 # ------------------------------------------------------------------------------
 
 export LESS="--tabs=4 -RFX"
-export FZF_DEFAULT_COMMAND="rg --files --no-ignore -g '!out/'"
 export _Z_CMD="jj"
 export _Z_DATA="$HOME/.cache/zdata"
 export EDITOR="vim"
--- source/fzf.sh
+++ source/fzf.sh
@@ -3,3 +3,6 @@
 # ------------------------------------------------------------------------------
 
 [ -f ~/.fzf.bash ] && source ~/.fzf.bash
+
+# I think the goal here was just to exclude the content of the 'out' directory.
+# export FZF_DEFAULT_COMMAND="rg --files --no-ignore -g '!out/'"
