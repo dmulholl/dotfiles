205df86a6e93ee2486ab859243c7b1ed20e374f8
Darren Mulholland <dmulholl@tcd.ie>
2023-03-31 00:57:31 +0100

    Use Python 3.11 installed from python.org

--- source/path.sh
+++ source/path.sh
@@ -30,6 +30,11 @@ if test -d "$HOME/.cargo/bin"; then
     PATH="$PATH:$HOME/.cargo/bin"
 fi
 
+# Python 3.11 installed from python.org.
+if test -d "/Library/Frameworks/Python.framework/Versions/3.11/bin"; then
+    PATH="$PATH:/Library/Frameworks/Python.framework/Versions/3.11/bin"
+fi
+
 # Macports binaries.
 if test -d "/opt/local/bin"; then
     PATH="$PATH:/opt/local/bin:/opt/local/sbin"
