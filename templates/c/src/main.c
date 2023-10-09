fc17c32853739341f96ef7df354360c06a7565a7
Darren Mulholland <dmulholl@tcd.ie>
2023-10-09 14:44:17 +0100

    Update C template

--- templates/c/src/main.c
+++ templates/c/src/main.c
@@ -17,6 +17,17 @@
 // POSIX.
 #include <unistd.h>
 
+typedef int8_t    i8;
+typedef uint8_t   u8;
+typedef int32_t   i32;
+typedef uint32_t  u32;
+typedef int64_t   i64;
+typedef uint64_t  u64;
+typedef float     f32;
+typedef double    f64;
+typedef ptrdiff_t isize;
+typedef size_t    usize;
+
 int main(int argc, char** argv) {
     printf("hello world!\n");
     return 0;
