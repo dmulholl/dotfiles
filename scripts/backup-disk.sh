20d2feba9d677fc69bb9daad7e22bce50f50e019
Darren Mulholland <dmulholl@tcd.ie>
2024-09-22 11:10:08 +0100

    Housekeeping

--- scripts/backup-disk.sh
+++ scripts/backup-disk.sh
@@ -1,40 +1,31 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-# Make sure we have a single argument.
-if test "$#" = "0"; then
-    echo "Error: too few arguments, a disk name is required."
-    exit 1
-elif test "$#" != "1"; then
-    echo "Error: too many arguments, expected 1, found $#."
+if test "$#" != "1"; then
+    echo "Error: expected 1 argument (the backup disk name), found $#."
     exit 1
 fi
 
-# Get the backup name. Defaults to the lowercased hostname.
+disk_name="$1"
+termtitle green "Rsync: ${HOME} --> ${disk_name}:${backup_name}"
+
 backup_name=$(hostname -s | tr '[:upper:]' '[:lower:]')
 if test -n "$DOT_BACKUP_NAME"; then
     backup_name="$DOT_BACKUP_NAME"
 fi
 
-# The argument specifies the name of the destination disk.
-disk_name="$1"
-termtitle green "Rsync: ${HOME} --> ${disk_name}:${backup_name}"
-
-# The slash at the end of the source directory path is important. It means
-# copy the *contents* of the directory.
+# The slash at the end means copy the contents of the directory.
 src="${HOME}/"
 
-# A slash at the end of the destination path can cause problems when copying
-# to the root of a drive.
-dst="/Volumes/${disk_name}/backups/${backup_name}"
+# A slash at the end can cause problems when copying to the root of a drive.
+dst="/Volumes/${disk_name}/Backups/${backup_name}"
 
-# Check the destination directory exists.
 if test ! -d $dst; then
     echo "Error: cannot locate $dst."
     exit 1
 fi
 
-# Reset the maximum number of files that the application can have open.
+# Reset the maximum number of files the process can have open.
 ulimit -n 4096
 
 # Run rsync in mirroring mode.
