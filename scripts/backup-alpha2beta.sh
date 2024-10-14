8c47a7462a858951f7c516a950d4aecf5f06f130
Darren Mulholland <dmulholl@tcd.ie>
2024-10-14 17:39:36 +0100

    Add extra backup command

---
+++ scripts/backup-alpha2beta.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+termtitle red "Backing up unique content from Alpha (SSD) to Beta (DHD) using rsync."
+
+# The slash at the end of the source directory path is important. It means
+# copy the *contents* of the directory.
+src="/Volumes/Alpha/"
+
+# A slash at the end of the destination path can cause problems when copying
+# to the root of a drive.
+dst="/Volumes/Beta/Backups/alpha"
+
+# Check the destination directory exists.
+if test ! -d $dst; then
+    echo "Error: cannot locate $dst."
+    exit 1
+fi
+
+# Increase the maximum number of files the process can have open.
+ulimit -n 4096
+
+# Run rsync in mirroring mode.
+rsync -av --delete \
+    --exclude ".Spotlight*" \
+    --exclude ".Trashes" \
+    --exclude ".[Tt]rash" \
+    --exclude ".fseventd" \
+    --exclude ".fseventsd" \
+    --exclude ".DS_Store" \
+    --exclude ".TemporaryItems" \
+    --exclude "/Backups/" \
+    --exclude ".DocumentRevisions-V100" \
+    -- "$src" "$dst"
